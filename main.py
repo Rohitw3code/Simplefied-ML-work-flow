65eab758e3a2179eb6f5bb98d62a02ac4741c1cc
Beluga <rohitcode005@gmail.com>
2023-10-15 15:51:48 +0530

    updated

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import pandas as pd
+import tempfile
 
-# Initializing Flask
 app = Flask(__name__, template_folder='templates', static_folder='static')
+
+app.config['UPLOAD_FOLDER'] = tempfile.mkdtemp() 
 
 
 @app.route("/")
@@ -12,14 +14,23 @@ def home():
 @app.route('/upload', methods=['GET', 'POST'])
 def load():
     if request.method == "POST":
-        f = request.files['file']
-        print(f)
-        return pd.read_csv(f).head()
-
-
-    return "hello"
-
+        input_value = request.form.get("input_value")
+        if input_value == None:
+            input_value = 5
+        # f = request.files['file']
+        f = "204-795.csv"
+        df = pd.read_csv(f)
+        shape = df.shape
+        df = df.head(int(input_value)) 
+        return render_template(
+            'ana.html',
+            column_names=df.columns.values,
+            row_data=df.values.tolist(),
+            shape=shape,
+            zip=zip
+        )
+    
 
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5001)
+    app.run(debug=True, port=5001)
