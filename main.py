abab6e1f97d9459c4f9e5b5d5639f095af791411
rohitw3code <rohitcode005@gmail.com>
2023-10-17 16:18:02 +0530

    updated

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
 import pandas as pd
 import tempfile
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-app.config['UPLOAD_FOLDER'] = tempfile.mkdtemp() 
+meta = {}
+f = "204-795.csv"
 
 
 @app.route("/")
@@ -14,16 +15,12 @@ def home():
 @app.route('/upload', methods=['GET', 'POST'])
 def load():
     if request.method == "POST":
-        input_value = request.form.get("input_value")
-        if input_value == None:
-            input_value = 5
-        # f = request.files['file']
-        f = "204-795.csv"
+        input_value = request.form.get("input_value", 5)
         df = pd.read_csv(f)
         shape = df.shape
-        df = df.head(int(input_value)) 
+        df = df.head(int(input_value))
         return render_template(
-            'ana.html',
+            'workflow.html',
             column_names=df.columns.values,
             row_data=df.values.tolist(),
             shape=shape,
@@ -32,5 +29,16 @@ def load():
     
 
 
+@app.route('/add_value', methods=['POST'])
+def add_value():
+    print("Hello appended")
+    value = request.form.get("new_value")
+    print("value : ",value)
+    # app.config['my_list'].append(value)
+    return redirect('/upload') 
+        
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
+    # app.run(host='192.168.123.135', port=5001, debug=True, threaded=False)    
